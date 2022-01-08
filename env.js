2908840fd73bc835b42b188acad07a948877f32e
long <long0908113045@gmail.com>
2022-01-08 11:32:55 +0700

    update

--- env.js
+++ env.js
@@ -1,7 +1,14 @@
-class env{
-    static hostFE = "http://localhost:4200";
-    static hostBE = "https://backend-bookshop-kltn.herokuapp.com";
-    static partnerCode = "MOMOIYF420211121";
-    static accessKey = "hU9agyEwyvIf2AjK";
-    static secretKey = "asIb5x2pjYn2FQYHQXv8PYBNWH4h4C9F";
+
+const hostFE = "http://localhost:4200";
+const hostBE = "https://backend-bookshop-kltn.herokuapp.com";
+const partnerCode = "MOMOIYF420211121";
+const accessKey = "hU9agyEwyvIf2AjK";
+const secretKey = "asIb5x2pjYn2FQYHQXv8PYBNWH4h4C9F";
+
+module.exports = {
+    hostFE :hostFE,
+    hostBE:hostBE,
+    partnerCode:partnerCode,
+    accessKey:accessKey,
+    secretKey:secretKey
 }
