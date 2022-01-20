4418361d08de1f2529cb359a4b59d9cac9224092
long <long0908113045@gmail.com>
2022-01-20 12:07:50 +0700

    update

--- env.js
+++ env.js
@@ -1,5 +1,6 @@
 
-const hostFE = "http://bookstorewebsite.s3-website-us-east-1.amazonaws.com";
+// const hostFE = "http://bookstorewebsite.s3-website-us-east-1.amazonaws.com";
+const hostFE = "http://localhost:4200";
 const hostBE = "https://backend-bookshop-kltn.herokuapp.com";
 const partnerCode = "MOMOIYF420211121";
 const accessKey = "hU9agyEwyvIf2AjK";
